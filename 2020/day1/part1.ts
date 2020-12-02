fb9e481bda1ca558fee20d33df66544114de7c73
Daniel Power <me@danielpower.ca>
2020-12-02 19:12:26 -0330

    Prettier

--- 2020/day1/part1.ts
+++ 2020/day1/part1.ts
@@ -1,11 +1,11 @@
-import {readLines} from "https://deno.land/std/io/mod.ts";
+import { readLines } from "https://deno.land/std/io/mod.ts";
 import * as path from "https://deno.land/std/path/mod.ts";
 
 const values: number[] = [];
 const filename = path.join(Deno.cwd(), "input.txt");
 const fileReader = await Deno.open(filename);
 
-for await (let line of readLines(fileReader)) {
+for await (const line of readLines(fileReader)) {
   values.push(parseInt(line, 10));
 }
 
