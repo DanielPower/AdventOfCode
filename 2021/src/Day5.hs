85460b00c5ec6c5aa934c69445ecccd09f12e045
Daniel Power <me@danielpower.ca>
2021-12-06 03:39:53 -0330

    Day 5 Part 2

--- 2021/src/Day5.hs
+++ 2021/src/Day5.hs
@@ -6,7 +6,6 @@ where
 
 import Data.List.Split (splitWhen)
 import Data.Map (Map, elems, empty, findWithDefault, insert)
-import Debug.Trace (trace, traceShow)
 
 isValidVentLine :: [[Int]] -> Bool
 isValidVentLine line = do
@@ -14,8 +13,22 @@ isValidVentLine line = do
   let [x2, y2] = last line
   x1 == x2 || y1 == y2
 
+isValidVentLine2 :: [[Int]] -> Bool
+isValidVentLine2 line = do
+  let [x1, y1] = head line
+  let [x2, y2] = last line
+  x1 == x2 || y1 == y2 || (abs (x1 - x2) == abs (y1 - y2))
+
 ventLineToPoints :: [[Int]] -> [(Int, Int)]
-ventLineToPoints line = do
+ventLineToPoints line
+  | x1 == x2 || y1 == y2 = straightLine line
+  | otherwise = diagonalLine line
+  where
+    [x1, y1] = head line
+    [x2, y2] = last line
+
+straightLine :: [[Int]] -> [(Int, Int)]
+straightLine line = do
   let [x1, y1] = head line
   let [x2, y2] = last line
   let minX = min x1 x2
@@ -24,6 +37,15 @@ ventLineToPoints line = do
   let maxY = max y1 y2
   [(x, y) | x <- [minX .. maxX], y <- [minY .. maxY]]
 
+diagonalLine :: [[Int]] -> [(Int, Int)]
+diagonalLine line = do
+  let [x1, y1] = head line
+  let [x2, y2] = last line
+  let minX = min x1 x2
+  let minY = min y1 y2
+  let maxX = max x1 x2
+  let maxY = max y1 y2
+  zip [x1, x1 + (signum $ x2 - x1)..x2] [y1, y1 + (signum $ y2 - y1)..y2] -- Source: https://stackoverflow.com/a/30063912
 
 countVentLines :: Map (Int, Int) Int -> [(Int, Int)] -> Map (Int, Int) Int
 countVentLines counts [] = counts
@@ -33,30 +55,36 @@ countVentLines counts points = do
   let newCounts = insert point newCountForPoint counts
   countVentLines newCounts (tail points)
 
+parseInput :: [String] -> ([[Int]] -> Bool) -> [(Int, Int)]
+parseInput inputLines lineValidator =
+  concatMap
+  ventLineToPoints
+  ( filter
+      lineValidator
+      ( map
+          ( map
+              ( map (read :: String -> Int)
+                  . splitWhen (== ',')
+              )
+              . filter (/= "->")
+              . splitWhen (== ' ')
+          )
+          inputLines
+      )
+  )
+
 part1 :: IO String -> IO ()
 part1 input = do
   inputLines <- lines <$> input
-  let ventPoints =
-        concatMap
-          ventLineToPoints
-          ( filter
-              isValidVentLine
-              ( map
-                  ( map
-                      ( map (read :: String -> Int)
-                          . splitWhen (== ',')
-                      )
-                      . filter (/= "->")
-                      . splitWhen (== ' ')
-                  )
-                  inputLines
-              )
-          )
+  let ventPoints = parseInput inputLines isValidVentLine
   let ventLineCounts = countVentLines empty ventPoints
-  let ventLineIntersections = filter (>1) (elems ventLineCounts)
+  let ventLineIntersections = filter (> 1) (elems ventLineCounts)
   print (length ventLineIntersections)
 
 part2 :: IO String -> IO ()
 part2 input = do
   inputLines <- lines <$> input
-  print "Not implemented"
+  let ventPoints = parseInput inputLines isValidVentLine2
+  let ventLineCounts = countVentLines empty ventPoints
+  let ventLineIntersections = filter (> 1) (elems ventLineCounts)
+  print (length ventLineIntersections)
