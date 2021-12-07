067e91a539964d90c37ba07f88141f243e4082f0
Daniel Power <me@danielpower.ca>
2021-12-06 21:42:23 -0330

    Day 6 Parts 1 and 2

--- 2021/app/Main.hs
+++ 2021/app/Main.hs
@@ -5,6 +5,7 @@ import Day2
 import Day3
 import Day4
 import Day5
+import Day6
 import System.Environment
 
 days =
@@ -12,7 +13,8 @@ days =
     ("day2.txt", [Day2.part1, Day2.part2]),
     ("day3.txt", [Day3.part1, Day3.part2]),
     ("day4.txt", [Day4.part1, Day4.part2]),
-    ("day5.txt", [Day5.part1, Day5.part2])
+    ("day5.txt", [Day5.part1, Day5.part2]),
+    ("day6.txt", [Day6.part1, Day6.part2])
   ]
 
 main :: IO ()
