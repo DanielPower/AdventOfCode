282c61691b81fe2ad11d32c5f19ee5b969248807
Daniel Power <me@danielpower.ca>
2023-11-23 20:52:56 -0330

    Update 2021for new structure

--- 2021/haskell/app/Main.hs
+++ 2021/haskell/app/Main.hs
@@ -11,14 +11,14 @@ import Day8
 import System.Environment
 
 days =
-  [ ("day1.txt", [Day1.part1, Day1.part2]),
-    ("day2.txt", [Day2.part1, Day2.part2]),
-    ("day3.txt", [Day3.part1, Day3.part2]),
-    ("day4.txt", [Day4.part1, Day4.part2]),
-    ("day5.txt", [Day5.part1, Day5.part2]),
-    ("day6.txt", [Day6.part1, Day6.part2]),
-    ("day7.txt", [Day7.part1, Day7.part2]),
-    ("day8.txt", [Day8.part1, Day8.part2])
+  [ ("01.in", [Day1.part1, Day1.part2]),
+    ("02.in", [Day2.part1, Day2.part2]),
+    ("03.in", [Day3.part1, Day3.part2]),
+    ("04.in", [Day4.part1, Day4.part2]),
+    ("05.in", [Day5.part1, Day5.part2]),
+    ("06.in", [Day6.part1, Day6.part2]),
+    ("07.in", [Day7.part1, Day7.part2]),
+    ("08.in", [Day8.part1, Day8.part2])
   ]
 
 main :: IO ()
@@ -28,4 +28,4 @@ main = do
   let (file, solutions) = days !! day
   let part = (read (args !! 1) :: Int) - 1
   let solution = solutions !! part
-  solution (readFile ("inputs/" ++ file))
+  solution (readFile ("../inputs/" ++ file))
