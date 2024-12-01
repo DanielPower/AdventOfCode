f4d17057dae0f3bb5756bf82c1582b5880b62888
Daniel Power <me@danielpower.ca>
2024-11-30 21:53:23 -0330

    Make async

--- generate.ts
+++ generate.ts
@@ -1,5 +1,5 @@
 import { readdirSync } from "node:fs";
-import { Year } from "./types.ts";
+import { Project, Year } from "./types.ts";
 import { build, run, test } from "./util.ts";
 
 const yearDirectories = readdirSync(".").filter((f) => f.match(/^\d{4}$/));
@@ -10,39 +10,41 @@ const years: Array<[string, Year]> = await Promise.all(
   ]),
 );
 
+const getProjectResult = async (
+  project: Project,
+  year: string,
+  language: string,
+  day: number,
+  part: number,
+) => {
+  try {
+    if (!project.exists(`${year}/${language}`, day, part)) {
+      return;
+    }
+    await build(project, year, language, day, part);
+    const { stdout } = await run(project, year, language, day, part);
+    const { result, actual, expected } = await test(year, day, part, stdout!);
+    const dayString = day.toString().padStart(2, "0");
+    if (result) {
+      console.log(`✅ ${year}: Day ${dayString} Part ${part}: Success`);
+    } else {
+      console.log(`❌ ${year}: Day ${dayString} Part ${part}: Failure`);
+      console.log(`Expected: ${expected}`);
+      console.log(`Recevied: ${actual}`);
+    }
+  } catch (e) {
+    console.log(`❌ ${year}: Day ${day} Part ${part}: Failure`);
+    console.log(`Unexpected error`);
+    console.log(e);
+  }
+};
+
 years.forEach(([year, projects]) => {
-  Object.entries(projects).forEach(async ([language, project]) => {
+  Object.entries(projects).forEach(([language, project]) => {
+    const promises: Promise<void>[] = [];
     for (let day = 1; day <= 25; day += 1) {
       for (let part = 1; part <= 2; part += 1) {
-        try {
-          if (!project.exists(`${year}/${language}`, day, part)) {
-            continue;
-          }
-          const buildResult = await build(project, year, language, day, part);
-          if (!buildResult) {
-            console.log(`❌ ${year}: Day ${day} Part ${part}: Build Failure`);
-            continue;
-          }
-          const { stdout } = await run(project, year, language, day, part);
-          const { result, actual, expected } = await test(
-            year,
-            day,
-            part,
-            stdout!,
-          );
-          const dayString = day.toString().padStart(2, "0");
-          if (result) {
-            console.log(`✅ ${year}: Day ${dayString} Part ${part}: Success`);
-          } else {
-            console.log(`❌ ${year}: Day ${dayString} Part ${part}: Failure`);
-            console.log(`Expected: ${expected}`);
-            console.log(`Recevied: ${actual}`);
-          }
-        } catch (e) {
-          console.log(`❌ ${year}: Day ${day} Part ${part}: Failure`);
-          console.log(`Unexpected error`);
-          console.log(e);
-        }
+        promises.push(getProjectResult(project, year, language, day, part));
       }
     }
   });
