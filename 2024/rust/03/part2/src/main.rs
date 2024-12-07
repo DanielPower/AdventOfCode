93932d1930665bef86d24a21d1b27b01508bec27
Daniel Power <me@danielpower.ca>
2024-12-07 14:01:24 -0330

    2024 Day 3 Part 2: Rust cleanup

--- 2024/rust/03/part2/src/main.rs
+++ 2024/rust/03/part2/src/main.rs
@@ -1,12 +1,6 @@
 use regex::Regex;
 use std::io::Read;
 
-const DO: usize = 1;
-const DONT: usize = 2;
-const MUL: usize = 3;
-const MUL_A: usize = 4;
-const MUL_B: usize = 5;
-
 fn main() {
     let mut enabled = true;
     let mut total = 0;
@@ -14,18 +8,20 @@ fn main() {
     let mut buffer = String::new();
     std::io::stdin().lock().read_to_string(&mut buffer).unwrap();
 
-    let re = Regex::new(r"(do\(\))|(don't\(\))|(mul\((\d+),(\d+)\))").unwrap();
-    for foo in re.captures_iter(buffer.as_str()) {
-        if let Some(_) = foo.get(DO) {
-            enabled = true;
+    let re =
+        Regex::new(r"(?<do>do\(\))|(?<dont>don't\(\))|(?<mul>mul\((?<mul_a>\d+),(?<mul_b>\d+)\))")
+            .unwrap();
+    for caps in re.captures_iter(buffer.as_str()) {
+        if let Some(_) = caps.name("do") {
+            enabled = true
         }
-        if let Some(_) = foo.get(DONT) {
+        if let Some(_) = caps.name("dont") {
             enabled = false;
         }
-        if let Some(_) = foo.get(MUL) {
+        if let Some(_) = caps.name("mul") {
             if enabled {
-                let a = foo.get(MUL_A).unwrap().as_str().parse::<i32>().unwrap();
-                let b = foo.get(MUL_B).unwrap().as_str().parse::<i32>().unwrap();
+                let a = caps.name("mul_a").unwrap().as_str().parse::<i32>().unwrap();
+                let b = caps.name("mul_b").unwrap().as_str().parse::<i32>().unwrap();
                 total += a * b;
             }
         }
