54e6e03c77b8ace4c90f919858f305053c80c5ac
Daniel Power <me@danielpower.ca>
2024-12-08 03:33:06 -0330

    2024 Day 7: Rust

---
+++ 2024/rust/07/part1/src/main.rs
@@ -0,0 +1,36 @@
+use std::io::BufRead;
+
+fn evaluate(acc: usize, mut values: Vec<usize>, result: usize) -> bool {
+    if values.is_empty() {
+        return acc == result;
+    }
+    let value = values.pop().unwrap();
+    evaluate(acc + value, values.clone(), result) || evaluate(acc * value, values.clone(), result)
+}
+
+fn main() {
+    let mut total = 0;
+    let re = regex::Regex::new(r"^(?<result>\d+): (?<values>\d+(?: \d+)*)").unwrap();
+    for line in std::io::stdin().lock().lines() {
+        for cap in re.captures_iter(&line.unwrap()) {
+            let result = cap
+                .name("result")
+                .unwrap()
+                .as_str()
+                .parse::<usize>()
+                .unwrap();
+            let values = cap
+                .name("values")
+                .unwrap()
+                .as_str()
+                .split_whitespace()
+                .map(|v| v.parse::<usize>().unwrap())
+                .rev()
+                .collect::<Vec<_>>();
+            if evaluate(0, values, result) {
+                total += result;
+            }
+        }
+    }
+    println!("Total: {}", total);
+}
