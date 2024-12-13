3ca7ed9b4def5d0730d53347099456c2cb38b2f9
Daniel Power <me@danielpower.ca>
2024-12-12 21:46:53 -0330

    Add comments to describe solution

--- 2024/rust/12/part2/src/main.rs
+++ 2024/rust/12/part2/src/main.rs
@@ -46,15 +46,37 @@ fn flood_fill(
     visited[y as usize][x as usize] = true;
     let neighbors = get_valid_neighbors(x, y, garden);
     let mut area = 1;
+    // The perimeter is equal to the number of corners. This set of cases can determine the number
+    // of corners based on the number and placement of neighbors.
     let mut perimeter = match neighbors.len() {
+        // A lonely plant is surounded by 4 corners
+        // o-o
+        // |#|
+        // o-o
         0 => 4,
+
+        // A plant with one neighbor has 2 corners
+        // o-
+        // |##
+        // o-
         1 => 2,
+
         2 => {
             if neighbors[0].0 == neighbors[1].0 || neighbors[0].1 == neighbors[1].1 {
+                // A plant with 2 neighbors has no corners if they are in a line
+                // ---
+                // ###
+                // ---
                 0
             } else {
                 let dx = neighbors[0].0 as i32 + neighbors[1].0 as i32 - (x as i32) * 2;
                 let dy = neighbors[0].1 as i32 + neighbors[1].1 as i32 - (y as i32) * 2;
+                // A plant with 2 neighbors has 1 or 2 corners if they are not in a line
+                // If the inner diagonal (marked with ?) is in the same region, there is 1 corner.
+                // Otherwise there are 2 corners.
+                // o--
+                // |##
+                // |#?
                 if garden[y as usize][x as usize] == garden[(y + dy) as usize][(x + dx) as usize] {
                     1
                 } else {
@@ -63,8 +85,16 @@ fn flood_fill(
             }
         }
         3 => {
+            // A plant with 3 neighbors has 0 to 2 corners, depending on the diagonals in the
+            // direction of the T intersection (marked with ?).
+            // |#?
+            // |##
+            // |#?
             let ax = neighbors[0].0 + neighbors[1].0 + neighbors[2].0 - x * 3;
             let ay = neighbors[0].1 + neighbors[1].1 + neighbors[2].1 - y * 3;
+
+            // By taking the average of the three neighbors, we can determine the direction of the
+            // T intersection. We can use this to find the two relevant diagonals.
             let (n1x, n1y, n2x, n2y) = if ax == 0 {
                 (x - 1, y + ay, x + 1, y + ay)
             } else {
@@ -77,6 +107,12 @@ fn flood_fill(
                 })
                 .count()
         }
+
+        // A plant with 4 neighbors has one corner for each diagonal (marked with ?) neighbor that is
+        // in a different region.
+        // ?#?
+        // ###
+        // ?#?
         4 => [
             (x - 1, y - 1),
             (x - 1, y + 1),
